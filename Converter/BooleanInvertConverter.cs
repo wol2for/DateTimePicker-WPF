ea9a1f4f6fc0aad28abf797ec0114eb2a35fc5e1
wol2for <30880764+wol2for@users.noreply.github.com>
2018-08-27 17:34:12 +0300

    Update BooleanInvertConverter.cs

--- Converter/BooleanInvertConverter.cs
+++ Converter/BooleanInvertConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace TestCollspaned.Converter
+namespace DateTimePickerWPF.Converter
 {
     public class BooleanInvertConverter : IValueConverter
     {
