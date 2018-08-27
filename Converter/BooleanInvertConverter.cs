6853a4a678177044f9b9995738ec90cbd7380986
wol2for <wol2for@hotmail.com>
2018-08-27 17:19:54 +0300

    main

---
+++ Converter/BooleanInvertConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TestCollspaned.Converter
+{
+    public class BooleanInvertConverter : IValueConverter
+    {
+        public bool IsInverted { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value as bool?);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value as bool?);
+        }
+    }
+}
